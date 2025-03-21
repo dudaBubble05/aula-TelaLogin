aa5336ba2f5057ff75ecea2963df2e97cffa6883
Maria Eduarda <maria.freitas.eduarda694@gmail.com>
2025-03-21 09:52:16 -0300

    Fazendo Página de Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Cabecalho from './components/Cabecalho/Cabecalho';
+import LoginForm from './components/LoginForm/LoginForm';
 import Rodape from './components/Rodape/Rodape';
 import Welcome from './components/Welcome/Welcome';
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       <Cabecalho/>
-      <Welcome />
+      <LoginForm />
       <Rodape/>
     </>
   )
